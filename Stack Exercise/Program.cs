04a0167f56403bc9496438e1c899d559bde92619
yassa98 <yasirudeshan3@gmail.com>
2021-12-02 15:36:00 +0530

    first commit

---
+++ Stack Exercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Stack_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            Stack _stack = new Stack();
+
+            for(i=1; i<=5; i++)
+            {
+                _stack.Push(i);
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("--------------");
+
+            for (i = 0; i < 5; i++)
+            {
+                Console.WriteLine(_stack.Pop());
+            }
+
+            _stack.Clear();
+
+        }
+    }
+}
---
+++ Stack Exercise/Stack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack_Exercise
+{
+    class Stack
+    {
+        private readonly List<object> items = new List<object>();
+        public void Push(object obj)
+        {
+            if( obj == null)
+                throw new InvalidOperationException("Items cannot be null");
+          
+            items.Add(obj);
+        }
+        public object Pop()
+        {
+            if (items.Count == 0)
+                throw new InvalidOperationException("No items in the stack");
+
+            var index = items.Count - 1;
+
+            var remover = items[index];
+
+            items.RemoveAt(index);
+
+            return remover;
+        }
+
+        public void Clear()
+        {
+            if (items.Count == 0)
+                throw new InvalidOperationException("No items in the stack");
+
+            items.Clear();
+
+        }
+    }
+}
